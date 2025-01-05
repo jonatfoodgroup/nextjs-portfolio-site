7c8a46b254c98f2d326dd566eb539652d9a05e66
Jon Senterfitt <jon.senterfitt@thefoodgroup.com>
2025-01-04 20:37:57 -0500

    Updates

--- app/components/article/ArticleCard.js
+++ app/components/article/ArticleCard.js
@@ -4,9 +4,9 @@ import AuthorCard from "./AuthorCard";
 
 const ArticleCard = ({ article }) => {
     return (
-        <div className="bg-white shadow-lg rounded-lg p-8 flex flex-col h-full" >
+        <div className="bg-white shadow-lg rounded-lg p-8 flex flex-col h-full text-start" >
             <Link href={`/blog/articles/${article.slug}`}>
-                <h2 className="text-xl font-bold text-dark-blue mb-2">
+                <h2 className="text-xl text-start font-bold text-dark-blue mb-2">
                     {article.title.rendered}
                 </h2>
                 <p className="text-md text-dark-blue mt-0 line-clamp-2" dangerouslySetInnerHTML={{ __html: article.excerpt.rendered }}>
